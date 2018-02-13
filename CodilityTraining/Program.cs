7cf81043674e0b20f320433dfe2fcf45b1f2df40
Tarcisio Luna <tarluna@gmail.com>
2018-02-13 16:51:52 -0300

    Initial commit

---
+++ CodilityTraining/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodilityTraining {
+    class Program {
+        static void Main (string[] args) {
+            System.Console.WriteLine (BinaryGap (529));
+        }
+
+        public static int BinaryGap (int N) {
+            string binary = Convert.ToString (N, 2);
+            int result = 0, current = 0;
+
+            for (int i = 0; i < binary.Length; i++) {
+                if (binary[i] != '0') {
+                    result = Math.Max (result, current);
+                    current = 0;
+                } else {
+                    current++;
+                }
+            }
+            return result;
+        }
+    }
+}
